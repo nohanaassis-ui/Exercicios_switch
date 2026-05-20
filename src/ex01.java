2d39baf25b95583f40e64428d660e8d88f6a80b9
nohanaassis-ui <nohana.assis@edu.senai.br>
2026-05-20 16:17:09 -0300

    Dia da semana

---
+++ src/ex01.java
@@ -0,0 +1,41 @@
+public class ex01 {}
+
+void main() {
+    int dia =7;
+    switch (dia) {
+
+
+        case 1:
+            IO.println("Hoje é domingo");
+            break;
+
+        case 2:
+            IO.println("Hoje é segunda");
+            break;
+        case 3:
+            IO.println("Hoje é terça");
+            break;
+
+        case 4:
+            IO.println("Hoje é quarta");
+            break;
+
+        case 5:
+            IO.println("Hoje é quinta");
+            break;
+
+        case 6:
+            IO.println("Hoje é sexta");
+            break;
+
+        case 7:
+            IO.println("Hoje é sabado");
+            break;
+
+        default:
+            IO.println("Digite um dia existente");
+    }
+
+}
+
+
